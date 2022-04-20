382351a35a85e1b18e5c927b43aa5b2e00e4b6a1
DannyelYankee <73075185+DannyelYankee@users.noreply.github.com>
2022-04-20 13:55:29 +0200

    phpRegistro empezado

--- app/src/main/java/com/example/myfitnessnotebook/phpRegistro.java
+++ app/src/main/java/com/example/myfitnessnotebook/phpRegistro.java
@@ -19,7 +19,6 @@ public class phpRegistro extends Worker {
     public Result doWork() {
         String user = getInputData().getString("user");
         String password = getInputData().getString("password");
-
         String server = "http://ec2-18-132-60-229.eu-west-2.compute.amazonaws.com/djuape001/WEB/registro.php";
 
         return null;
